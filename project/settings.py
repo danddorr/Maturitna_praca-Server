bc353c2248b2197a3e8665581ed5346669c807f6
danddorr <120369188+danddorr@users.noreply.github.com>
2024-11-21 10:12:18 +0100

    restrict allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = [os.getenv("DOMAIN_NAME", "127.0.0.1")]
 
 # Application definition
 
